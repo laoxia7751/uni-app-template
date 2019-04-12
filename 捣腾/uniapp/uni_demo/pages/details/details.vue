2a7040ce2e17a089a3eb27a55cea4e45033d3123
xiajitao <290548940@qq.com>
2019-04-12 18:30:53 +0800

    第一次提交,未完成

---
+++ 捣腾/uniapp/uni_demo/pages/details/details.vue
@@ -0,0 +1,30 @@
+<template>
+	<view>
+		123
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				film:{}
+			};
+		},
+		mounted() {
+			this.getFileDetails()
+		},
+		onLoad(options) {
+			console.log(options);
+		},
+		methods:{
+			getFileDetails(id){
+				
+			}
+		}
+	}
+</script>
+
+<style>
+
+</style>
