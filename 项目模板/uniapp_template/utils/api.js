e6412b7c5fcc2179a533bc1dd9da85279f6c0795
xiajitao <290548940@qq.com>
2019-08-02 16:18:19 +0800

    clear

--- 项目模板/uniapp_template/utils/api.js
+++
@@ -1,9 +0,0 @@
-//接口地址
-export default {
-    webHost: "http://dev-api.pure-international.com:8888/",
-    apiHost: "https://douban.uieee.com/",
-    apiList: {
-        //用户登录
-        test: 'v2/movie/in_theaters',
-    }
-}
